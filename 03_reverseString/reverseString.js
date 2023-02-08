7246127a8cb2eab4fee53e71c9638b2349b1541a
Rarstyak <rarstyak@gmail.com>
2023-02-08 10:26:45 -0500

    Solve 1-5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let value = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        value += str[i];
+    }
+    return value;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    let value = array;
+    let aLen = array.length;
+    let rLen = remove.length;
+    backScroll: for (let i = aLen - 1; i >= 0; i--) {
+        for (let j = 0; j < rLen; j++) {
+            if (value[i] === remove[j]) {
+                console.log(`value: ${value}\narray[i]: ${array[i]}\nremove[j]: ${remove[j]}`);
+                value.splice(i, 1);
+                continue backScroll;
+            }
+        }
+    }
+    return value;
 };
 
 // Do not edit below this line
