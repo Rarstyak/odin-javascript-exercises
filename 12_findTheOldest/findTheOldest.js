3e6c06ff9ac88b4572bbcc11490144373d0295c0
Rarstyak <rarstyak@gmail.com>
2023-02-16 15:39:07 -0500

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
 const findTheOldest = function(people) {
-    currentYear = new Date().getFullYear();
+    let currentYear = new Date().getFullYear();
+    let sortedPeople = people.sort(
+        (a,b) => 
+            (a.yearOfDeath === undefined ? currentYear : a.yearOfDeath) - a.yearOfBirth 
+            < (b.yearOfDeath === undefined ? currentYear : b.yearOfDeath) - b.yearOfBirth 
+            ? 1 : -1
+    );
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
